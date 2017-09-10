8ebcd4710a4c364193e0329c43477b3b1ddf46c6
Jason Fong <jfong27@calpoly.edu>
2017-09-09 22:34:19 -0700

    new perform intent

--- advice.js
+++ advice.js
@@ -2,10 +2,65 @@
 module.exports = {
    'ADVICE_EN_GB': {
       'bee sting': 'apply ice or something',
-      'asdfasd': 'aggg'
+      'cut': 'bandage',
+      'burn' : 'somethin',
+      'acne': 'a',
+      'allergies': '',
+      'panic attack': '',
+      'anxiety': '',
+      'arthritis': '',
+      'asthma': '',
+      'autism': '',
+      'back pain': '',
+      'cancer': '',
+      'carpal tunnel syndrome': '',
+      'carpal tunnel': '',
+      'cold': '',
+      'flu': '',
+      'fever': '',
+      'cough': '',
+      'depression': '',
+      'diabetes': '',
+      'ear infection': '',
+      'eczema': '',
+      'epilepsy': '',
+      'seizure': '',
+      'erectile dysfunction': '',
+      'food poisoning': '',
+      'herpes': '',
+      'heart attack': '',
+      'heartburn': '',
+      'irritable bowel syndrome': '',
+      'IBS': '',
+      'kidney stone': '',
+      'knee pain': '',
+      'schizophrenia': '',
+      'stroke': '',
+      'splinter': '',
+      'sprain': '',
+      'lice': '',
+      'sunburn': '',
+      'sore throat': '',
+      
    },
    'ADVICE_EN_US': {
       'bee sting': 'apply ice or something',
-      'ww': 'ammmm'
+      'cut': 'bandage',
+      'asdfasd': 'aggg',
+      'burn' : 'somethin',
+      'gum' : 'jan'
+   },
+   
+   'ACTIONS_EN_GB': {
+   	  'CPR': 'ah ah ah',
+   	  'heimlich maneuver': 'jj',
+   	  'aaa': 'bbb'
+   
+   },
+   'ACTIONS_EN_US': {
+   	  'CPR': 'ah ah ah',
+   	  'heimlich maneuver': 'jj',
+   	  'aaa': 'bbb'
+   
    }
 };
--- index.js
+++ index.js
@@ -5,36 +5,43 @@
 const Alexa = require('alexa-sdk');
 const advice = require('./advice');
 
+
 const APP_ID = 'amzn1.ask.skill.98820a47-4974-4302-9ab6-8c17338ba5a9'; // TODO replace with your app ID (OPTIONAL).
 
 const languageStrings = {
     'en': {
         translation: {
             ADVICE: advice.ADVICE_EN_US,
+            ACTIONS: advice.ACTIONS_EN_US,
             SKILL_NAME: 'First Aid Advice',
             WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, how do you treat a bee sting? ... Now, what can I help you with?",
             WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
-            DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
-            HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
-            HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            DISPLAY_CARD_TITLE: '%s  - Treatment for %s.',
+            HELP_MESSAGE: "You can ask questions such as, how do you treat a cut, or, you can say exit...Now, what can I help you with?",
+            HELP_REPROMPT: "You can say things like, how do you cure diarrhea, or you can say exit...Now, what can I help you with?",
             STOP_MESSAGE: 'Goodbye!',
             ADVICE_REPEAT_MESSAGE: 'Try saying repeat.',
             ADVICE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
-            ADVICE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
-            ADVICE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
+            ADVICE_NOT_FOUND_WITH_ITEM_NAME: 'how to treat %s. ',
+            ADVICE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that ailment. ',
             ADVICE_NOT_FOUND_REPROMPT: 'What else can I help with?',
+            
+            ACTION_NOT_FOUND_WITH_ITEM_NAME: 'how to perform %s. ',
+            ACTION_NOT_FOUND_WITHOUT_ITME_NAME: 'how to do that. '
         },
     },
     'en-US': {
         translation: {
             ADVICE: advice.ADVICE_EN_US,
-            SKILL_NAME: 'First Aid Advice',
+            ACTIONS: advice.ACTIONS_EN_US,
+            SKILL_NAME: 'First Aid Advice'
         },
     },
     'en-GB': {
         translation: {
             ADVICE: advice.ADVICE_EN_US,
-            SKILL_NAME: 'First Aid Advice',
+            ACTIONS: advice.ACTIONS_EN_US,
+            SKILL_NAME: 'First Aid Advice'
         },
     }
 };
@@ -51,8 +58,47 @@ const handlers = {
         this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
     },
+    'PerformIntent': function (){
+    	const actionSlot = this.event.request.intent.slots.action;
+    	let actionName;
+    	if (actionSlot && actionSlot.value){
+    		actionName = actionSlot.value.toLowerCase();
+    	}
+    	
+    	const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), actionName);
+    	const myAction = this.t('ACTIONS');
+    	const advice = myAction[actionName];
+    	
+    	if (advice) {
+            this.attributes.speechOutput = advice;
+            this.attributes.repromptSpeech = this.t('ADVICE_REPEAT_MESSAGE');
+
+            this.response.speak(advice).listen(this.attributes.repromptSpeech);
+            this.response.cardRenderer(cardTitle, advice);
+            this.emit(':responseReady');
+        } else {
+            let speechOutput = this.t('ADVICE_NOT_FOUND_MESSAGE');
+            const repromptSpeech = this.t('ADVICE_NOT_FOUND_REPROMPT');
+            if (actionName) {
+                speechOutput += this.t('ACTION_NOT_FOUND_WITH_ITEM_NAME', actionName);
+            } else {
+                speechOutput += this.t('ACTION_NOT_FOUND_WITHOUT_ITEM_NAME');
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes.speechOutput = speechOutput;
+            this.attributes.repromptSpeech = repromptSpeech;
+
+            this.response.speak(speechOutput).listen(repromptSpeech);
+            this.emit(':responseReady');
+        }
+    	
+    
+    
+    },
     'AidIntent': function () {
-        const itemSlot = this.event.request.intent.slots.Item;
+        
+        const itemSlot = this.event.request.intent.slots.illness;
         let itemName;
         if (itemSlot && itemSlot.value) {
             itemName = itemSlot.value.toLowerCase();
@@ -62,12 +108,12 @@ const handlers = {
         const myAdvice = this.t('ADVICE');
         const advice = myAdvice[itemName];
 
-        if (recipe) {
-            this.attributes.speechOutput = recipe;
+        if (advice) {
+            this.attributes.speechOutput = advice;
             this.attributes.repromptSpeech = this.t('ADVICE_REPEAT_MESSAGE');
 
-            this.response.speak(recipe).listen(this.attributes.repromptSpeech);
-            this.response.cardRenderer(cardTitle, recipe);
+            this.response.speak(advice).listen(this.attributes.repromptSpeech);
+            this.response.cardRenderer(cardTitle, advice);
             this.emit(':responseReady');
         } else {
             let speechOutput = this.t('ADVICE_NOT_FOUND_MESSAGE');
@@ -85,6 +131,7 @@ const handlers = {
             this.response.speak(speechOutput).listen(repromptSpeech);
             this.emit(':responseReady');
         }
+        
     },
     'AMAZON.HelpIntent': function () {
         this.attributes.speechOutput = this.t('HELP_MESSAGE');
@@ -111,7 +158,8 @@ const handlers = {
         this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
         this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
-    },
+    }
+
 };
 
 exports.handler = function (event, context, callback) {
