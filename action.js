d12c89d77a1e353e90af551660e508b24333becd
Jason Fong <jfong27@calpoly.edu>
2017-09-10 23:17:29 -0700

    actions to perform

---
+++ action.js
@@ -0,0 +1,11 @@
+module.exports = {
+   'ACTIONS_EN_GB': {
+   	  'CPR': 'ah ah ah',
+   	  'heimlich maneuver': 'jj'
+   },
+   'ACTIONS_EN_US': {
+   	  'CPR': 'ah ah ah',
+   	  'heimlich maneuver': 'jj'
+   
+   }
+};
